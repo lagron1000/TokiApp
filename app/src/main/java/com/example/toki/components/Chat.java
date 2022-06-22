0a9c7da3788d0c91400cbccd6990d7ae6bf37185
Nofar Bart <nofar.bart@gmail.com>
2022-06-22 10:40:37 +0300

    Firebase get messages into chat

--- app/src/main/java/com/example/toki/components/Chat.java
+++ app/src/main/java/com/example/toki/components/Chat.java
@@ -1,12 +1,23 @@
-package com.example.toki;
+package com.example.toki.components;
 
+import android.content.BroadcastReceiver;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.example.toki.AppDB;
+import com.example.toki.R;
+import com.example.toki.components.Contacts;
+
+import Models.Contact;
+
 public class Chat extends AppCompatActivity {
+    private Contact receiverContact;
+    private AppDB dataBase;
+    private BroadcastReceiver broadcastReceiver;
+    public static final String NOTIFY_CHAT_ACTION = "notify_chat";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +27,9 @@ public class Chat extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(2500);
         animationDrawable.setExitFadeDuration(5000);
         animationDrawable.start();
+    }
+
+    private void loadContact() {
+//        receiverContact = (Contact) getIntent().getSerializableExtra()
     }
 }
--- app/src/main/java/com/example/toki/components/Contacts.java
+++ app/src/main/java/com/example/toki/components/Contacts.java
@@ -1,5 +1,5 @@
 package com.example.toki.components;
-
+import java.util.ArrayList;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,9 +8,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.toki.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.*;
+
+import Models.Contact;
+
 public class Contacts extends AppCompatActivity {
 
+    List<Contact> contacts = new ArrayList<Contact>();
 
+    Contacts () {
+        Contact Lior = new Contact("1","Lior", "1", "2");
+        Contact Nofar = new Contact("2","Nofar", "1", "3");
+        Contact Maya = new Contact("3","Maya","1","1");
+        contacts.add(Lior);
+        contacts.add(Nofar);
+        contacts.add(Maya);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +36,9 @@ public class Contacts extends AppCompatActivity {
             Intent i = new Intent(this, SearchContacts.class);
             startActivity(i);
         });
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
     }
 }
